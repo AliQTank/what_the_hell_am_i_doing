61d1f4f85a86a518d04b21cd8fc2b326cf507bdd
AliQTank <104526578+AliQTank@users.noreply.github.com>
2022-04-29 13:38:02 -0500

    Update app_4_meditatiion_2nd_Try.js

--- app_4_meditatiion_2nd_Try.js
+++ app_4_meditatiion_2nd_Try.js
@@ -2,38 +2,50 @@ const app = () => {
     const song = document.querySelector(".song");
     const play = document.querySelector(".play");
     const outline = document.querySelector(".moving-outline circle");
-    const video = document.querySelector(".vid-container video");
+    const video = document.querySelector('.vid-container video');
+
 
     //Sounds
-
-    const sounds = document.querySelectorAll(".sound-picker button");
-    //Time Display
+    const sounds = document.querySelectorAll(".sound-picker button")
+    // Time Display
     const TimeDisplay = document.querySelector(".time-display");
-    const timeSelect = document.querySelectorAll(".time-select button");
     //Get the length of the outline
     const outLineLength = outline.getTotalLength();
     console.log(outLineLength);
     //Duration
     let fakeDuration = 600;
 
-    outline.style.strokeDasharray = outLineLength;
-    outline.style.strokeDashoffset = outLineLength;
-
-    //play sound
-    play.addEventListener("click", ()=>{
+        outline.style.strokeDasharray = outLineLength;
+        outline.style.strokeDashoffset = outLineLength;
+        
+        
+//Play sound
+    play.addEventListener("click", () => {
         checkPlaying(song);
-    })
+    });
 
+    //select sound
 
-    //Select sound
     timeSelect.forEach(Option =>{
-        Option.addEventListener("click", function()  {
+        Option.addEventListener("click", function(){
             fakeDuration = this.getAttribute("data-time");
             TimeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`;
         })
     })
-    
-    
+
+    //Create a function specific to stop and play yhe sounds
+    const checkPlaying = song => {
+        if(song.paused) {
+            song.play();
+            video.play();
+            play.src = "./svg/pause.svg";
+        }else{
+            song.pause();
+            video.pause();
+            play.src = './svg/play.svg';
+        }
+    }
+    }
     //create a function to stop/play
 
 const checkPlaying = song => {
