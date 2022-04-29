54fecd3ec0c48466f6980f917189d769c57b946b
AliQTank <104526578+AliQTank@users.noreply.github.com>
2022-04-29 12:47:54 -0500

    Update app_4_meditatiion_2nd_Try.js

--- app_4_meditatiion_2nd_Try.js
+++ app_4_meditatiion_2nd_Try.js
@@ -20,9 +20,22 @@ const app = () => {
 
     //play sound
     play.addEventListener("click", ()=>{
-        song.play();
+        checkPlaying(song);
     })
     
+    //create a function to stop/play
+
+const checkPlaying = song => {
+    if(song.paused){
+        song.play();
+        video.play();
+        play.src = "./svg/pause.svg";
+    }else{
+        song.pause();
+        video.pause();
+        play.src = "./svg/play.svg";
+    }
+}
 }
 
 app();
